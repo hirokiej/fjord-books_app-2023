3579bf9bc18fc5455fadb3f6e342030d014e9744
hirokiej <123009866+hirokiej@users.noreply.github.com>
2024-06-28 01:06:54 +0900

    パスワード再設定のメール送信と一部国際化をしました。

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   root "books#index"
   resources :books
   resources :users, only: [:show, :index]
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
