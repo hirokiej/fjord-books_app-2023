a091fdf2a9823323972c637cd1cadefb1e4a616c
hirokiej <123009866+hirokiej@users.noreply.github.com>
2024-07-01 01:56:12 +0900

    Active Storageで画像アップロードを実装しました

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_one_attached :avatar do |attachable|
+    attachable.variant :thumb, resize_to_limit: [200, 200]
+  end
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
