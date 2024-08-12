fd24aab26324e93bc3e7110a57a5981a705a27b1
hirokiej <123009866+hirokiej@users.noreply.github.com>
2024-08-12 14:27:02 +0900

    migrationファイルを１つにまとめ、コメント投稿者表示に関するviewを修正しました

--- db/migrate/20240706170309_create_comments.rb
+++ db/migrate/20240706170309_create_comments.rb
@@ -3,7 +3,7 @@ class CreateComments < ActiveRecord::Migration[7.0]
     create_table :comments do |t|
       t.references :commentable, polymorphic: true, null: false
       t.text :content
-      t.integer :user_id
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
