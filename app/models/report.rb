5280a2b57264dec71b13b77218414e38b48150ba
hirokiej <123009866+hirokiej@users.noreply.github.com>
2024-08-20 18:58:56 +0900

    rubocopとerblintを通しました

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 class Report < ApplicationRecord
-  after_save :get_mentions_id
+  after_save :mentions_id
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 
-  has_many :active_mentions, class_name: 'Mention', foreign_key: :mentioning_report_id, dependent: :destroy
+  has_many :active_mentions, class_name: 'Mention', foreign_key: :mentioning_report_id, dependent: :destroy, inverse_of: :mentioning_report
   has_many :mentioning_reports, through: :active_mentions, source: :mentioned_report
 
-  has_many :passive_mentions, class_name: 'Mention', foreign_key: :mentioned_report_id, dependent: :destroy
+  has_many :passive_mentions, class_name: 'Mention', foreign_key: :mentioned_report_id, dependent: :destroy, inverse_of: :mentioned_report
   has_many :mentioned_reports, through: :passive_mentions, source: :mentioning_report
 
   validates :title, presence: true
@@ -22,14 +22,14 @@ class Report < ApplicationRecord
     created_at.to_date
   end
 
-  def get_mentions_id
-    Mention.where(mentioning_report_id: self.id).destroy_all
-    urls = self.content.scan(/http:\/\/localhost:3000\/reports\/\d+/)
-    if urls.any?
-      urls.each do |url|
-        id = url.match(/reports\/(\d+)/)[1]
-        Mention.create(mentioning_report_id: self.id, mentioned_report_id: id)
-      end
+  def mentions_id
+    Mention.where(mentioning_report_id: id).destroy_all
+    urls = content.scan(%r{http://localhost:3000/reports/\d+})
+    return unless urls.any?
+
+    urls.each do |url|
+      mentioned_id = url.match(%r{reports/(\d+)})[1]
+      Mention.create(mentioning_report_id: id, mentioned_report_id: mentioned_id)
     end
   end
 end
