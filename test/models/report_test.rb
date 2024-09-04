663e45c36236f2dda750e71738a85744f55d5724
hirokiej <123009866+hirokiej@users.noreply.github.com>
2024-09-05 00:53:14 +0900

    モデルテストを書き、rubocopとerblintを通しました。

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -6,4 +6,14 @@ class ReportTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test '#editanble' do
+    assert reports(:alice).editable?(users(:alice))
+  end
+
+  test '#created_on' do
+    travel_to Time.zone.local(2024, 9, 1, 12, 0, 0)
+    reports(:alice).created_at = Time.current
+    assert_equal Date.new(2024, 9, 1), reports(:alice).created_on
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,4 +6,8 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test '#name_or_email' do
+    assert_equal 'Alice', users(:alice).name_or_email
+  end
 end
